b78346593d61567c77cffeafe278e1487a33e522
veroena <veronica.enamorado@gmail.com>
2019-05-26 12:19:55 +0200

    Add docs folder

---
+++ docs/precache-manifest.8cd5fa8752fb3fa6309c7cd4a5a9934d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8397f3ffb25f26df4d49d1727b89f16e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff40723960ce855e710",
+    "url": "/static/css/main.ab29ca41.chunk.css"
+  },
+  {
+    "revision": "fc983541d07f29ec1041",
+    "url": "/static/js/2.3a8e5690.chunk.js"
+  },
+  {
+    "revision": "2ff40723960ce855e710",
+    "url": "/static/js/main.1f97d09e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
