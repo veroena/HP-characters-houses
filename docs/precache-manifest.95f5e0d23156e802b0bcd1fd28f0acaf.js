30557716aad244d9ec82becb842eced58c6c26a5
veroena <veronica.enamorado@gmail.com>
2019-05-26 22:25:00 +0200

    Deploy to GitHub

---
+++ docs/precache-manifest.95f5e0d23156e802b0bcd1fd28f0acaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130496ffdf115ea685eea25cf4b3893e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0032a4c0f156670e8c75",
+    "url": "./static/css/main.ab29ca41.chunk.css"
+  },
+  {
+    "revision": "fc983541d07f29ec1041",
+    "url": "./static/js/2.3a8e5690.chunk.js"
+  },
+  {
+    "revision": "0032a4c0f156670e8c75",
+    "url": "./static/js/main.dad134fa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
