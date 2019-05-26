466d2b0f521c100438918883f08bdcecbd7ec9c1
veroena <veronica.enamorado@gmail.com>
2019-05-26 13:21:53 +0200

    Change fetch URL and build

---
+++ docs/precache-manifest.edd2868a9f04e9c0b42fa298c816d0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dce29abd91c78fd68560d160359e32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28b05bd87b6d2e96ff70",
+    "url": "./static/css/main.ab29ca41.chunk.css"
+  },
+  {
+    "revision": "fc983541d07f29ec1041",
+    "url": "./static/js/2.3a8e5690.chunk.js"
+  },
+  {
+    "revision": "28b05bd87b6d2e96ff70",
+    "url": "./static/js/main.65cb6252.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
